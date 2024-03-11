7ef568f4c313e3d481f5a3d9c950bc66dc15c636
FaithOgendi <faithbrianna01@gmail.com>
2024-03-11 11:05:24 +0300

    completed assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+    all= []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        if owner is not None:
+            owner.add_pet(self)
+        self.owner = owner
+        self.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be an instance of Pet class")
+        self._pets.append(pet)
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda x: x.name)
+
+owner= Owner("Brianna")
